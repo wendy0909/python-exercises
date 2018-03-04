903395aa470d2d64a3163561f54c7946b426d9ec
wendy <qingyutx@gmail.com>
2018-03-04 05:41:34 +0000

    exercise1

--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,7 @@
 name = input('What is your name? ')
-print('Hello, ' + name)
+print('Hello, ' + name +'!"')
     
+name = input('What is your name? ')
+print(('Hello, ' + name).upper())
+print(('Your name has ' + str(len(name)) + ' letters in it! Awesome!').upper())
+
