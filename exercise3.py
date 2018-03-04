9a11131bd8ff0822abe36c95e676acd214fb9f73
wendy <qingyutx@gmail.com>
2018-03-04 05:27:05 +0000

    exercise3

---
+++ exercise3.py
@@ -0,0 +1,12 @@
+names = ['George', 'W', 'Bush']
+
+for dude in names:
+    for character in dude:
+      print(character)
+    
+name = 'Geoege W Bush'
+for character in name:
+    print(character.upper(),end=' ')
+    
+print(' ')
+    
