b2faa0e6f14cdb94bf5a6b60da63d1c4e1bcf437
wendy <qingyutx@gmail.com>
2018-03-05 04:37:53 +0000

    updated

--- exercise1.py
+++ exercise1.py
@@ -13,3 +13,9 @@ print("{0}'s favorit subject in school is {1}.".format(name, subject))
 day = int(input('Day (0-6)? '))
 dayOfWeek = ['Sunday', 'Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday']
 print(dayOfWeek[day])
+
+day = int(input('Day (0-6)? '))
+if day == 0 or day == 6:
+  print("Sleep in")
+else:
+  print("Go to work")
