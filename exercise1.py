f4431a63689c81b59214aeb1966c3d957cbb65ad
wendy <qingyutx@gmail.com>
2018-03-05 03:30:54 +0000

    updated file

--- exercise1.py
+++ exercise1.py
@@ -5,3 +5,7 @@ name = input('What is your name? ')
 print(('Hello, ' + name).upper())
 print(('Your name has ' + str(len(name)) + ' letters in it! Awesome!').upper())
 
+print("________'s favorite subject in school is ________.")
+name = input('What is name?')
+subject = input('What is subject?')
+print("{0}'s favorit subject in school is {1}.".format(name, subject))
